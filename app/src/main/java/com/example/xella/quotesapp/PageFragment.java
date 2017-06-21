fc733f025a5e92e06086e2a324150920080f3730
xella <xella.design@gmail.com>
2017-06-21 18:13:01 +0200

    Quote class implemented; Added styles for fragment layout; Intent to share quotes added;

--- app/src/main/java/com/example/xella/quotesapp/PageFragment.java
+++ app/src/main/java/com/example/xella/quotesapp/PageFragment.java
@@ -1,16 +1,15 @@
 package com.example.xella.quotesapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import org.w3c.dom.Text;
-import java.util.Random;
 
 /**
  * Created by Xella on 19-Jun-17.
@@ -22,16 +21,13 @@ public class PageFragment extends Fragment {
     int pageNumber;
     int backgroundColor;
 
-    String[] backgroundColorArray = {
-        "0x222222", "0x666666", "0x000000"
-    };
-
-    static PageFragment newInstance(int page, String quoteText, String quoteAuthor) {
+    static PageFragment newInstance(int page, String quoteText, String quoteAuthor, String color) {
         PageFragment pageFragment = new PageFragment();
         Bundle arguments = new Bundle();
         arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
         arguments.putString("Quotation Text", quoteText);
         arguments.putString("Quotation Author", quoteAuthor);
+        arguments.putString("Color", color);
         pageFragment.setArguments(arguments);
         return pageFragment;
     }
@@ -40,28 +36,38 @@ public class PageFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
-
-        Random rnd = new Random();
-        int randomIndex = rnd.nextInt(backgroundColorArray.length - 1);
-        Log.v("new bgcolor", backgroundColorArray[randomIndex]);
-        //backgroundColor = Color.parseColor(backgroundColorArray[randomIndex]);
-        //backgroundColor = Color.argb(60, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-
+        backgroundColor = Color.parseColor(getArguments().getString("Color"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment, null);
 
+        final String quoteText = getArguments().getString("Quotation Text");
+        final String quoteAuthor = getArguments().getString("Quotation Author");
+
         TextView quoteTextView = (TextView) view.findViewById(R.id.quote_text);
-        quoteTextView.setText(getArguments().getString("Quotation Text"));
+        quoteTextView.setText(quoteText);
 
         TextView quoteAuthorTextView = (TextView) view.findViewById(R.id.quote_author);
-        quoteAuthorTextView.setText(getArguments().getString("Quotation Author"));
+        quoteAuthorTextView.setText(quoteAuthor);
 
         LinearLayout quotePage = (LinearLayout) view.findViewById(R.id.quote_page);
         quotePage.setBackgroundColor(backgroundColor);
 
+        ImageButton shareButton = (ImageButton) view.findViewById(R.id.share_button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(android.content.Intent.EXTRA_TEXT, quoteText + " \n" + "Written by " + quoteAuthor);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
+
+
